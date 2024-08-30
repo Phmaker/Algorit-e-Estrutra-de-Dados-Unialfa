c644bd52c41c0004b9dd29a7295865fe784c8b92
Pedromaker <162760498+Phmaker@users.noreply.github.com>
2024-08-30 20:12:46 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    
+char letra1;
+int numeroint;
+float numerofloat;
+double double01;
+    
+    letra1 = 'C';
+    numeroint = 10;
+    numerofloat = 3.14f;
+    double01 = 17.928351;
+        
+        
+        printf("%c\n", letra1);
+        printf("%i\n", numeroint);
+        printf("%.2f\n", numerofloat);
+        printf("%f\n", double01);
+        
+    return 0;
+}
